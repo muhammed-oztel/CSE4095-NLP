70aac99cbf988b62b1a72db6b03e55e67504d1a0
alibrahimzada <alibrahimzada@gmail.com>
2022-04-05 13:53:41 +0300

    updated pipeline

--- Delivery-1/pipeline.sh
+++ Delivery-1/pipeline.sh
@@ -7,4 +7,19 @@ python main.py --clean_data True --raw_data_dir 2021-01 --f_name dataset
 python main.py --export_ngrams True --f_name dataset
 
 # extract collocations based on frequency
+python main.py --method frequency --f_name dataset
+
+# extract collocations based on mutual information
+python main.py --method pmi --f_name dataset
+
+# extract collocations based on t-test
+python main.py --method t_test --f_name dataset
+
+# extract collocations based on diff mean variance
+python main.py --method diff_mean_var --f_name dataset
+
+# extract collocations based on chi-square test
 python main.py --method chi_square --f_name dataset
+
+# extract collocations based on likelihood ratios
+python main.py --method likelihood_ratios --f_name dataset
