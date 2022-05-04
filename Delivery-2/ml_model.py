6bdad16f783273df8dd8bff4d87bc77779b67963
HuzeyfeAyaz <huzeyfeayaz23@hotmail.com>
2022-05-04 11:22:25 +0300

    MLModel class is simplified

--- Delivery-2/ml_model.py
+++ Delivery-2/ml_model.py
@@ -1,6 +1,5 @@
-from sklearn.metrics import f1_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 import pickle
 import os
 import json
@@ -17,28 +16,29 @@ class MLModel:
         os.makedirs(f'results/{model_name}', exist_ok=True)
 
     def vectorize(self):
-        if os.path.isfile('vectorizer.sav'):
-            self.vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
+        if os.path.isfile('vectorizer.h5'):
+            with open('vectorizer.h5', 'rb') as f:
+                self.vectorizer = pickle.load(f)
             self.X = self.vectorizer.transform(self.X)
             return
 
         self.vectorizer = TfidfVectorizer()
         self.X = self.vectorizer.fit_transform(self.X)        
-        filename = 'vectorizer.sav'
+        filename = 'vectorizer.h5'
         with open(filename, 'wb') as f:
             pickle.dump(self.vectorizer, f)
 
     def load_model(self):
-        if os.path.isfile(f'results/{self.model_name}/{self.model_name}.sav'):
-            with open(f'results/{self.model_name}/{self.model_name}.sav', 'rb') as f:
+        if os.path.isfile(f'results/{self.model_name}/{self.model_name}.h5'):
+            with open(f'results/{self.model_name}/{self.model_name}.h5', 'rb') as f:
                 self.model = pickle.load(f)
 
     def save_model(self):
-        filename = f'results/{self.model_name}/{self.model_name}.sav'
+        filename = f'results/{self.model_name}/{self.model_name}.h5'
         with open(filename, 'wb') as f:
             pickle.dump(self.model, f)
 
-    def _hyperparameter_tuning(self):
+    def train(self):
         self.gs = GridSearchCV(self.model, self.parameters, cv=10, scoring='f1_macro', n_jobs=-1)
         self.gs.fit(self.X, self.y)
 
@@ -46,35 +46,5 @@ class MLModel:
 
         with open(f'results/{self.model_name}/best_params.json', 'w') as f:
             json.dump(self.gs.best_params_, f)
-
-    def _k_fold_cv(self):
-        skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-        old_score = 0
-        res = {'results': []}
-        fold = 1
-        for train_index, test_index in skf.split(self.X, self.y):
-            print(f'Fold {fold}')
-            fold += 1
-            X_train, X_test = self.X[train_index], self.X[test_index]
-            y_train, y_test = self.y[train_index], self.y[test_index]
-
-            self.model.fit(X_train, y_train)
-
-            y_pred = self.model.predict(X_test)
-            score = f1_score(y_test, y_pred, average='macro')
-            res['results'].append(score)
-            
-            if score > old_score:
-                self.self.model = self.model
-                old_score = score
-        
-        with open(f'results/{self.model_name}/results.json', 'w') as f:
-            json.dump(res, f, indent=4)
-
-    def train(self):
-        if self.hyperparam_tuning:
-            self._hyperparameter_tuning()
-        else:
-            self._k_fold_cv()
 
         self.save_model()
--- Delivery-2/pipeline.sh
+++ Delivery-2/pipeline.sh
@@ -6,13 +6,13 @@ python main.py --clean_data True --raw_data_dir 2021-01 --f_name dataset
 # split data into training and test sets
 python main.py --split_data True
 
-# perform hyperparameter tuning on logistic regression
-python main.py --hyperparam_tuning True --model logistic_regression
+# perform logistic regression
+python main.py --model logistic_regression
 # evaluate logistic regression model on test set
 python main.py --model logistic_regression --test_model True
 
-# perform hyperparameter tuning on svm
-python main.py --hyperparam_tuning True --model svm
+# perform svm
+python main.py --model svm
 # evaluate svm model on test set
 python main.py --model svm --test_model True
 
@@ -21,7 +21,7 @@ python main.py --model multi_naive_bayes
 # evaluate multinomial naive bayes on test set
 python main.py --model multi_naive_bayes --test_model True
 
-# perform hyperparameter tuning on random forest
-python main.py --hyperparam_tuning True --model random_forest
+# perform random forest
+python main.py --model random_forest
 # evaluate random forest model on test set
 python main.py --model random_forest --test_model True
